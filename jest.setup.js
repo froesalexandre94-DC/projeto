731888563430292b10e3ffa3484031fdf384741c
froesalexandre94-DC <froesalexandre94@gmail.com>
2025-11-16 12:33:05 -0300

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,13 @@
 import '@testing-library/jest-dom';
+
+// Polyfill da Fetch API para Jest
+import "whatwg-fetch";
+
+// Garante Request / Response globais
+global.Request = Request;
+global.Response = Response;
+
+// Mock do Supabase
 jest.mock("@supabase/supabase-js", () => ({
   createClient: jest.fn(() => require("./tests/__mocks__/supabase.js").supabase)
 }));
-
