2331c758d9dea89b6830c0d7213fd2bb7147bfc4
froesalexandre94-DC <froesalexandre94@gmail.com>
2025-11-16 11:51:55 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 export default {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest'
+    "^.+\\.(js|jsx)$": ["babel-jest", { configFile: "./babel-jest.config.cjs" }],
   },
   extensionsToTreatAsEsm: [],
   moduleFileExtensions: ['js', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
+
