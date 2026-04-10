4563536bd6ce41d0d134f02e0fb96fa4f6efc763
froesalexandre94-DC <froesalexandre94@gmail.com>
2026-04-09 21:51:43 -0300

    Update supabase.js

--- tests/__mocks__/supabase.js
+++ tests/__mocks__/supabase.js
@@ -1,1 +1,13 @@
+export const supabase = {
+  from: jest.fn(() => supabase),
 
+  select: jest.fn(() => ({
+    data: [{ id: 1, nome: "Produto Teste", quantidade: 10 }],
+    error: null
+  })),
+
+  insert: jest.fn(() => ({
+    data: { id: 2, nome: "Cabo USB-C", quantidade: 5 },
+    error: null
+  }))
+};
