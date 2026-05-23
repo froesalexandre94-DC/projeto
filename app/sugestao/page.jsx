0e4e2e0f036bc21464432a2995b465076b120e55
froesalexandre94-DC <froesalexandre94@gmail.com>
2026-05-23 20:02:24 -0300

    Update page.jsx

--- app/sugestao/page.jsx
+++ app/sugestao/page.jsx
@@ -8,7 +8,7 @@ import React, {
 
 import { useRouter } from 'next/navigation';
 
-import { supabase } from '@lib/supabase';
+import { supabase } from '../..lib/supabase';
 
 export default function SugestoesCompraPage() {
   const router = useRouter();
