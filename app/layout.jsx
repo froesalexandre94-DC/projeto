351dcd61c330bb02b3fb346a6a4ceb30b37f425e
froesalexandre94-DC <froesalexandre94@gmail.com>
2026-05-23 20:02:49 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
-import './globals.css';
+import '../globals.css';
 import { Inter } from 'next/font/google';
-import AccessibilityPanelClient from './components/AccessibilityPanelClient';
+import AccessibilityPanelClient from '../components/AccessibilityPanelClient';
 
 const inter = Inter({ subsets: ['latin'] });
 
